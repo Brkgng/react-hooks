850945fd1b744e78de9c9b85d7d48734410111b9
Berker Gungor <yasarberkergungor@gmail.com>
2023-02-28 00:04:25 +0300

    feat: add solution for exercise 04 extras

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,75 +2,93 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
+import {useLocalStorageState} from '../utils'
 
-const initialSquares = Array(9).fill(null)
+const initialSquares = [Array(9).fill(null)]
 function Board() {
-  // 🐨 squares is the state for this component. Add useState for squares
-  const [squares, setSquares] = React.useState(initialSquares)
+  const [currentIndex, setCurrentIndex] = useLocalStorageState(
+    'tic-tac-toe:index',
+    0,
+  )
+  const [squares, setSquares] = useLocalStorageState(
+    'tic-tac-toe:history',
+    initialSquares,
+  )
 
-  // 🐨 We'll need the following bits of derived state:
-  // - nextValue ('X' or 'O')
-  // - winner ('X', 'O', or null)
-  // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
-  // 💰 I've written the calculations for you! So you can use my utilities
-  // below to create these variables
-  const nextValue = calculateNextValue(squares)
-  const winner = calculateWinner(squares)
-  const status = calculateStatus(winner, squares, nextValue)
+  const nextValue = calculateNextValue(squares[currentIndex])
+  const winner = calculateWinner(squares[currentIndex])
+  const status = calculateStatus(winner, squares[currentIndex], nextValue)
 
-  // This is the function your square click handler will call. `square` should
-  // be an index. So if they click the center square, this will be `4`.
   function selectSquare(square) {
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    if (winner || squares[square]) return null
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    const squaresCopy = [...squares]
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
+    const currentSquares = squares[currentIndex]
+    if (winner || currentSquares[square]) return null
+
+    const squaresCopy = [...currentSquares]
     squaresCopy[square] = nextValue
-    // 🐨 set the squares to your copy
-    setSquares(squaresCopy)
+
+    setSquares(squares => [...squares.slice(0, currentIndex + 1), squaresCopy])
+    setCurrentIndex(index => index + 1)
   }
 
   function restart() {
     setSquares(initialSquares)
+    setCurrentIndex(0)
   }
 
   function renderSquare(i) {
     return (
       <button className="square" onClick={() => selectSquare(i)}>
-        {squares[i]}
+        {squares[currentIndex][i]}
+      </button>
+    )
+  }
+
+  function renderHistory(index) {
+    let text = 'Go to '
+    if (index === 0) text += 'game start'
+    else text += `move #${index}`
+
+    if (index === currentIndex) text += ' (current)'
+    return (
+      <button
+        disabled={index === currentIndex}
+        onClick={() => setCurrentIndex(index)}
+      >
+        {text}
       </button>
     )
   }
 
   return (
-    <div>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
+    <div style={{display: 'flex', gap: 15}}>
+      <div>
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+        <button className="restart" onClick={restart}>
+          restart
+        </button>
       </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
+      <div className="status">
+        {status}
+        <ol>
+          {squares.map((key, index) => (
+            <li key={index}>{renderHistory(index)}</li>
+          ))}
+        </ol>
       </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-      <button className="restart" onClick={restart}>
-        restart
-      </button>
     </div>
   )
 }
